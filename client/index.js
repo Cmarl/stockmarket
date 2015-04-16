09f847468cf3b9ade0919de502489152b2c4c1f5
Clyde G Marl <marl.clyde@gmail.com>
2015-04-16 01:53:56 -0700

    add in styling

--- client/index.js
+++ client/index.js
@@ -3,35 +3,48 @@
 
 $(document).ready(init);
 
-var root, user, cash, symbol, buyAmnt, portfolios;
+var root, user, balance, symbol, buyAmnt, portfolios;
 
 function init(){
   root = new Firebase('https://stockmarket-app.firebaseio.com/');
   user = root.child('user');
-  user.on('value',updateInfo);
-  cash = root.child('cash');
+  user.on('value',updateName);
+  $('#update').click(newUser);
+  balance = root.child('balance');
+  balance.on('value',updateBalance);
   portfolios = root.child('portfolios');
   portfolios.on('child_added',addPortfolio);
   portfolios.on('child_changed',portfolioChanged);
   $('#createPortfolio').click(createPortfolio);
-  $('#update').click(updateInfo);
   $('#buyStock').on('click','#buy',setSymbol);
-  //showPortfolios();
   $('#buy').click(getQuote);
 }
 
-function portfolioChanged(snapshot){
-  var key = snapshot.key();
-  var portfolio = snapshot.val();
-  console.log(portfolios.child(key).val());
+function newUser(){
+  var name = $('#name').val();
+  var cash = $('#cash').val();
+  user.set(name);
+  balance.set(cash);
+  $('#input').hide();
+}
+
+function updateName(snapshot){
+  var name = snapshot.val();
+  $('#welcome').text(name);
+
+}
+
+function updateBalance(snapshot){
+  var bal = snapshot.val();
+  $('#showBalance').text(bal);
 }
 
 function addPortfolio(snapshot){
   //var portfolio = snapshot.val();
   var key = snapshot.val();
-  console.log(key.name);
+  console.log(snapshot.key())
   var portName = key.name;
-  var $option = '<option class="portfolio">'+portName+'</option>';
+  var $option = '<option class="portfolio" value="' + snapshot.key() + '">'+portName+'</option>';
   $('.portfolioList').append($option);
   var $div = $('<div class="'+portName+'">');
 
@@ -47,41 +60,55 @@ function createPortfolio(){
   });
 }
 
-function updatePortfolios(){
-  var portfolio = $('#portName').val();
-  root.create({portfolios: portfolio});
-  //showPortfolios();
-}
-
 function setSymbol(){
   symbol = $('#symbol').val();
-  console.log(symbol);
   buyAmnt = $('#buyAmnt').val();
-  console.log(buyAmnt);
 }
 
-function updateInfo(){
-  var name = $('#name').val();
-  var cash = $('#cash').val();
-  root.update({user: name, cash: cash});
-  $('#welcome').text('Welcome, '+name);
-  $('#showBalance').text('$ '+cash);
+function portfolioChanged(snapshot){
+  var portfolio = snapshot.key();
+
+  
+
+  // console.log(portfolio);
+  // console.log(portfolios.child(portfolio));
 }
+
+// function updatePortfolio(snapshot){
+//   var port = snapshot.val();
+//
+//   var symbol = snapshot.symbol;
+//   var amount = snapshot.amount;
+//   var position = snapshot.position;
+//
+//   var $stock = $('<div class="stock"></div>');
+//   var $sym = $('<h3>'+symbol+': '+amount+'</h3>');
+//   var $position = $('<h3 class="position"> Position: $'+position+'</h3>');
+//   $stock.append($sym);
+//   $stock.append($position);
+//   $(port).append($stock);
+//
+// }
 
 function getQuote(){
   var symbol = $('#symbol').val().toUpperCase();
   var url = 'http://dev.markitondemand.com/Api/v2/Quote/jsonp?symbol=' + symbol + '&callback=?';
   $.getJSON(url, function(response){
-    console.log(response);
     var price = response.LastPrice;
-    var amnt = $('#buyAmnt').val();
-    var $sym = $('<h3>'+ symbol + ': ' + amnt + '</h3>');
-    var $position = $('<h3 class="position"> Position: $' +(price * amnt).toFixed(2) + '</h3>');
-    var $selected = '.'+$('.portfolioList').val();
-    console.log($($selected));
-    $($selected).append($sym);
-    $($selected).append($position);
+    var amount = $('#buyAmnt').val();
+    //var $sym = $('<h3>'+ symbol + ': ' + amnt + '</h3>');
+    //var $position = $('<h3 class="position"> Position: $' +(price * amnt).toFixed(2) + '</h3>');
+    //var $selected = '.'+$('.portfolioList').val();
+    //var $stock = $('<div class="stock"></div>');
+    var portfolio = portfolios.child($('.portfolioList').val());
+    portfolio.push({
+                    symbol: symbol,
+                    amount: amount,
+                    position: ((price * amount).toFixed(2))
+                   });
+    //$stock.append($sym);
+    //$stock.append($position);
+    //$($selected).append($stock);
+    $('#showBalance').text(($('#showBalance').text() - price));
   });
 }
-
-//$('#quote').text(JSON.stringify(response));
